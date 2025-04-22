81363529551c2e092fb5bd7b37f54b1873800403
myshko <mr@ukr.net>
2025-04-22 17:04:25 +0300

    Module 15, SQL tracks task

---
+++ SQL (15)/tracks_1/tracks.py
@@ -0,0 +1,95 @@
+import sqlite3
+
+# conn = sqlite3.connect('trackdb.sqlite')
+# cur = conn.cursor()
+
+with sqlite3.connect('trackdb.sqlite') as conn:
+    cur = conn.cursor()
+
+# Make some fresh tables using executescript()
+cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+DROP TABLE IF EXISTS Genre;
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+                  
+CREATE TABLE Genre (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    genre_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER    
+);
+''')
+
+handle = open('tracks.csv')
+
+# Another One Bites The Dust,Queen,Greatest Hits,55,100,217103
+#   0                          1      2           3  4   5
+
+for line in handle:
+    line = line.strip();
+    pieces = line.split(',')
+    if len(pieces) < 7 : continue
+
+    name = pieces[0]
+    artist = pieces[1]
+    album = pieces[2]
+    count = pieces[3]
+    rating = pieces[4]
+    length = pieces[5]
+    genre = pieces[6]
+
+    # print(name, artist, album, count, rating, length, genre)
+
+    cur.execute('''INSERT OR IGNORE INTO Artist (name) 
+        VALUES ( ? )''', ( artist, ) )
+    cur.execute('SELECT id FROM Artist WHERE name = ? ', (artist, ))
+    artist_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Genre (name) 
+    VALUES ( ? )''', ( genre, ) )
+    cur.execute('SELECT id FROM Genre WHERE name = ? ', (genre, ))
+    genre_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR IGNORE INTO Album (title, artist_id) 
+        VALUES ( ?, ? )''', ( album, artist_id ) )
+    cur.execute('SELECT id FROM Album WHERE title = ? ', (album, ))
+    album_id = cur.fetchone()[0]
+
+    cur.execute('''INSERT OR REPLACE INTO Track
+        (title, album_id, genre_id, len, rating, count) 
+        VALUES ( ?, ?, ?, ?, ?, ? )''', 
+        ( name, album_id, genre_id, length, rating, count ) )
+
+    conn.commit()
+
+#genres = ['Rock', 'Metal']
+cur.execute('''SELECT Track.title, Artist.name, Album.title, Genre.name
+    FROM Track JOIN Genre JOIN Album JOIN Artist 
+    ON Track.genre_id = Genre.id AND Track.album_id = Album.id AND Album.artist_id = Artist.id
+    ORDER BY Artist.name LIMIT 3''')
+#    WHERE Genre.genre IN (?, ?) ORDER BY Genre.genre ASC, Artist.name ASC''', genres)
+songs = cur.fetchall()
+track_width = max(len(song[0]) for song in songs) + 5
+# print(f"Number of {', '.join(genres)} songs: {len(genre_songs)}")
+print(f"{'Track':<{track_width}}| {'Artist':<{20}}| {'Album':<{20}}| {'Genre':<{20}}")
+print('=' * 100)
+for song in songs:
+    print(f"{song[0]:<{track_width}}| {song[1]:<{20}}| {song[2]:<{20}}| {song[3]:<{20}}")
